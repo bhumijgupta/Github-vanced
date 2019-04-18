ce3e5e44dde57d0926b0ddd468c13969c335579e
bhumijgupta <bhumijgupta@gmail.com>
2019-04-18 09:53:53 +0530

    minor change

--- colorify.js
+++ colorify.js
@@ -1,16 +1,17 @@
-btn = document.querySelector(".js-profile-editable-edit-button");
+function colorify() {
+    // Find the contribution boxes
+    var contri = document.querySelectorAll("rect.day");
+    if (contri.length > 0) {
+        // if exists, change every color to green
+        contri.forEach((box) => {
+            box.setAttribute("fill", "#196127");
+        });
+    }
+}
+
+var btn = document.querySelector(".js-profile-editable-edit-button");
 // Check if it is the user profile by checking Edit profile button
 if (btn && btn.textContent === "Edit") {
     colorify();
 }
 
-function colorify() {
-    // Find the contribution boxes
-    contri = document.querySelectorAll("rect.day");
-    if (contri.length > 0) {
-        // if exists, change every color to green
-        contri.forEach(box => {
-            box.setAttribute('fill', '#196127')
-        })
-    }
-}
