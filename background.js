a8e275e439216ad33c3c71b6bce68b231795e42c
bhumijgupta <bhumijgupta@gmail.com>
2019-05-18 20:48:59 +0530

    Added .gitignore

--- background.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = {
-  config: {
-    contri: "enabled",
-    darkmode: "enabled"
-  }
-};
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set(defaultConfig, null);
-});
